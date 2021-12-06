4314253dfd0f6fbea44e161ee971084d56db410b
mjuan99 <54592648+mjuan99@users.noreply.github.com>
2021-12-06 11:13:01 -0300

    Initial commit

---
+++ neocomplexx.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+const fs = require("fs")
+const jsPackacge = JSON.parse(fs.readFileSync("package.json"))
+
+app.get('/', (req, res) => {
+    res.json({nombre: jsPackacge.name, version: jsPackacge.version})
+})
+
+app.listen(3000, () => {
+    console.log("Escuchando en el puerto 3000")
+})
