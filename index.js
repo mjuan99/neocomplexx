78cadca7a632953f3526d4ae61651eb426962127
mjuan99 <54592648+mjuan99@users.noreply.github.com>
2021-12-09 11:50:25 -0300

    npm run start y dotenv
    Se agrego el comando para levantar el servidor con "npm run start" y se utilizo dotenv para guardar el número de puerto en una variable de ambiente

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require("express")
 const app = express()
+const port = process.env.PORT
 
 const fs = require("fs")
 const jsPackacge = JSON.parse(fs.readFileSync("package.json"))
@@ -8,6 +10,6 @@ app.get('/health', (req, res) => {
     res.json({nombre: jsPackacge.name, version: jsPackacge.version})
 })
 
-app.listen(3000, () => {
-    console.log("Escuchando en el puerto 3000")
+app.listen(port, () => {
+    console.log("Escuchando en el puerto " + port)
 })
