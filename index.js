1d97cb4cc79ddff198c3bfe10f93f9b7c33c06c5
mjuan99 <54592648+mjuan99@users.noreply.github.com>
2021-12-17 11:43:29 -0300

    docker-mysql
    Se paso la información que devuelve el server a una base de datos mysql en un contenedor de docker

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-require('dotenv').config()
-const express = require("express")
-const app = express()
-const port = process.env.PORT
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
 
-const fs = require("fs")
-const jsPackacge = JSON.parse(fs.readFileSync("package.json"))
+const mysql = require('mysql2');
+const con = mysql.createConnection({host: "localhost", user: "root", password: "root", database: "neocomplexx"});
 
-app.get('/health', (req, res) => {
-    res.json({nombre: jsPackacge.name, version: jsPackacge.version})
-})
-
-app.listen(port, () => {
-    console.log("Escuchando en el puerto " + port)
+con.connect(err => {
+    if (err)
+        console.log("Error al conectarse a la base de datos");
+    else{
+        app.get('/health', (req, res) => {
+            con.query("SELECT * FROM registro", (err, result, fields) => {
+                if (!err)
+                    res.json({nombre: result[0].nombre, version: result[0].vers});
+                else
+                    res.send("Error de conexion")
+            })
+        });
+        
+        app.listen(port, () => {
+            console.log("Escuchando en el puerto " + port)
+        });
+    }
 })
